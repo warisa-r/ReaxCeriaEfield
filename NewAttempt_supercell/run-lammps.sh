466c9064ea3c98271215d6952d68c2480d549647
Andres Ramos <andres.ramos@graniewicz.com>
2024-06-27 23:40:28 +0200

    Same results for attempted recreation of Peters code with modern reax and supercell from ase

---
+++ NewAttempt_supercell/run-lammps.sh
@@ -0,0 +1,21 @@
+# REAX potential for CeO2 system
+# .....
+
+units		real
+
+atom_style	charge
+read_data	data.CeO2
+
+pair_style	reax 10.0 0 1 1.0e-6
+pair_coeff	* * ffield.reax 4 3
+
+neighbor	2 bin
+neigh_modify	every 10 delay 0 check no
+
+
+dump            1 all custom 10000 steps-ut.xyz id xu yu zu 
+dump            2 all custom 10000 steps-ut.CeO2 id xu yu zu q 
+
+
+minimize        1.0e-6 1.0e-8 1000 10000
+
---
+++ NewAttempt_supercell/run-modernreax-lammps.sh
@@ -0,0 +1,22 @@
+# REAX potential for CeO2 system
+# .....
+
+units		real
+
+atom_style	charge
+read_data	data.CeO2
+
+pair_style reaxff NULL checkqeq yes
+pair_coeff * * ffield.reax O Ce
+
+neighbor	2 bin
+neigh_modify	every 10 delay 0 check no
+
+fix qeq all qeq/reaxff 1 0.0 10.0 1.0e-6 reaxff
+
+dump            1 all custom 10000 steps-ut.xyz id xu yu zu 
+dump            2 all custom 10000 steps-ut.CeO2 id xu yu zu q 
+
+
+minimize        1.0e-6 1.0e-8 1000 10000
+
