c10e53c5641144805ed416c07de149125a4370fb
YiyangZhuu <yiyang.zhu@rwth-aachen.de>
2024-07-11 14:44:41 +0200

    doc, read run.py

--- reax_ff_sim/ceo2_reaxff_fluorite/run.py
+++ reax_ff_sim/ceo2_reaxff_fluorite/run.py
@@ -1,8 +1,15 @@
+# it's basically the same, but with old lammps
+# I tried to get the exactly same structure as in the paper, which is in geometry_file/new110slab
+# would like to get 110 correct first
+# surface energy is not crazy huge anymore, it's around 30-60 compare to paper around 1
+# assuming the mathformel is correct, and the unit conversion also correct
+# there might be some trick we don't know while creating structure
 from lammps import lammps
 import numpy as np
 
+# convertion factor
 conv1 = 4184 # kcal/mol to J/mol
-mol = 6.02214076e23
+mol = 6.02214076e23 # J/mol to J
 conv2 = 1e-20 # A^2 to m^2
 lmp_bulk = lammps()
 lmp_bulk.file("ceo2fluorite.lmp")
@@ -10,20 +17,19 @@ E_bulk = lmp_bulk.get_thermo("pe")
 
 
 def get111():
-    nBulk_111 = 224 #14
-    A_111 = 828.4211067 #51.7762   A^2
+    nBulk_111 = 224 #14 for 111slab
+    A_111 = 828.4211067 #51.7762 for 111slab  A^2
     lmp111 = lammps()
     lmp111.file("ceo2slab111.lmp")
     E_slab = lmp111.get_thermo("pe")
     E_surf_111 = (E_slab - nBulk_111 * E_bulk) * conv1 / (2 * A_111 * conv2 * mol) 
-    #E_surf_111 = (E_slab - nBulk_111 * E_bulk) / (2 * A_111) 
     print(E_bulk)
     print(E_slab)
     print(E_surf_111)
 
 def get110():
-    nBulk_110 = 312 #21/4 312
-    A_110 = 676.4022963 #79.2658 676.4022963 A^2
+    nBulk_110 = 312 #21/4 for 'new110slab', 312 for '110supercell'
+    A_110 = 676.4022963 #79.2658 for 'new110slab', 676.4022963 for '110supercell' A^2
     lmp110 = lammps()
     lmp110.file("ceo2slab110.lmp")
     E_slab = lmp110.get_thermo("pe")
