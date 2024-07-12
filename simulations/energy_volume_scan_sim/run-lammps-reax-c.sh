1b5b8d515a66b8cb9fd3cbf5eaad6dbf749d8515
Warisa <compund555@gmail.com>
2024-07-12 11:24:03 +0200

    add EV scan sim

---
+++ simulations/energy_volume_scan_sim/run-lammps-reax-c.sh
@@ -0,0 +1,21 @@
+# REAX potential for CeO2 system
+# .....
+boundary       p p p
+units		real
+
+atom_style	charge
+read_data data_1.69.CeO2
+
+pair_style reax/c NULL checkqeq yes
+
+pair_coeff * * ffield.reax O Ce
+
+neighbor	2 bin
+neigh_modify	every 10 delay 0 check no
+
+fix 1 all qeq/reax 1 0.0 10.0 1.0e-6 reax/c
+
+#dump            1 all custom 10000 steps-ut.xyz id xu yu zu 
+#dump            2 all custom 10000 steps-ut.CeO2 id xu yu zu q 
+
+minimize        1.0e-6 1.0e-8 1000 10000
