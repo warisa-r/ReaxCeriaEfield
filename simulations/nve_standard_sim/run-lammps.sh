f2cd492208e16bd4028a0b63aac6f4f7989dd0d6
Warisa <compund555@gmail.com>
2024-07-13 15:28:54 +0200

    update the format of dump file to be able to be visualized with vmd

--- simulations/nve_standard_sim/run-lammps.sh
+++ simulations/nve_standard_sim/run-lammps.sh
@@ -12,7 +12,7 @@ pair_coeff	* * ffield.reax 4 3
 neighbor	2 bin
 neigh_modify	every 10 delay 0 check no
 
-dump            1 all custom 10000 steps-ut.xyz id xu yu zu 
-dump            2 all custom 10000 steps-ut.CeO2 id xu yu zu q 
+dump            1 all custom 10000 steps-ut.lammpstrj id xu yu zu 
+dump            2 all custom 10000 steps-ut-with-charges.lammpstrj id xu yu zu q 
 
 minimize        1.0e-6 1.0e-8 1000 10000
