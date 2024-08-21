0ecb6cc0a48df582838ed3515f5e15ac08e0f626
Warisa <compund555@gmail.com>
2024-08-21 11:24:26 +0200

    update job script

--- simulations/npt_nvt_standard_sim/job_script.sh
+++ simulations/npt_nvt_standard_sim/job_script.sh
@@ -5,25 +5,19 @@
 #SBATCH --time=01:00:00
 #SBATCH --partition=batch
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --ntasks=8
+#SBATCH --cpus-per-task=1
 
 module load gcc
-module load OpenMPI
+module load openmpi/4.1.4
 module load CMake
+module load mpi4py
 module load Python/3.10
 
-# Create a virtual environment in the job's temporary directory
-python -m venv myenv
-
-# Activate the virtual environment
-source myenv/bin/activate
-
 # Install matplotlib within the virtual environment
+
+pip install mpi4py
 pip install matplotlib
 
 # Run the Python script
 srun python run.py
-
-# Deactivate the virtual environment after the script has finished
-deactivate
