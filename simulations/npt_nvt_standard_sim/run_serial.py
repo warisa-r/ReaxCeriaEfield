7aea1c440300f43645190df7e98333d6ad2f50e8
YiyangZhuu <yiyang.zhu@rwth-aachen.de>
2024-08-23 18:50:33 +0200

    apply mean  lattice constant to nvt

--- simulations/npt_nvt_standard_sim/run_serial.py
+++ simulations/npt_nvt_standard_sim/run_serial.py
@@ -1,66 +1,20 @@
 from lammps import lammps
-import matplotlib.pyplot as plt
-import statistics
 
 lmp = lammps()
+
+# Run lammps npt simulation
 lmp.file("run-lammps-npt.lmp")
 
-# Total number of timesteps to run for NPT
-total_steps = 10000
-steps_per_run = 100
+# Some method to calculate mean lattice constant
 
-# Lists to store time (steps) and lattice constants
-time_steps = []
-lx_values = []
-ly_values = []
-lz_values = []
-T_values = []
-
-# Initialize old lattice constants with None
-lx_old, ly_old, lz_old, T_old = None, None, None, None
-
-# Run the simulation in chunks and collect lattice constants
-for i in range(0, total_steps, steps_per_run):
-    lmp.command(f"run {steps_per_run} pre no post no")  # Run in chunks of 500 steps
-    
-    # Get the box dimensions after each chunk of 500 steps
-    lx_new = lmp.get_thermo("lx")
-    ly_new = lmp.get_thermo("ly")
-    lz_new = lmp.get_thermo("lz")
-    T_new = lmp.get_thermo("temp")
-
-    # Store the data
-    time_steps.append(i + steps_per_run)
-    lx_values.append(lx_new)
-    ly_values.append(ly_new)
-    lz_values.append(lz_new)
-    T_values.append(T_new)
-
-    # Update old lattice constants
-    lx_old, ly_old, lz_old, T_old = lx_new, ly_new, lz_new, T_new
-
-
-# Plotting the results
-plt.figure(figsize=(10, 6))
-plt.plot(time_steps, lx_values, label='lx', marker='o')
-plt.plot(time_steps, ly_values, label='ly', marker='s')
-plt.plot(time_steps, lz_values, label='lz', marker='^')
-plt.xlabel('Time Steps')
-plt.ylabel('Lattice Constant (Angstroms)')
-plt.title('Lattice Constant as a Function of Time')
-plt.legend()
-plt.grid(True)
-# Save the plot as a picture file
-plt.savefig('plot4.png')
-# Save the simulation results
-#lmp.command("write_restart restart.NPT")  # Save the restart file
-
-'''T_mean = statistics.fmean(T_values)
-plt.figure()
-plt.plot(time_steps, T_values)
-plt.xlabel('Time Steps')
-plt.ylabel('Temperatur')
-plt.title('Temperature as a Funktion of Time')
-plt.grid(True)
-plt.savefig('plot_T.png')
-print(T_mean)'''
+# Re-initialize lammps instance for NVT (optional, for continuation)
+lmp = lammps()
+# apply mean lattice constant to NVT ( TEST )
+_lx = 10.8
+_ly = 10.8
+_lz = 10.8
+# Create simulation box
+lmp.file("run-lammps-nvt.lmp")
+lmp.command(f"change_box all x final 0 {_lx} y final 0 {_ly} z final 0 {_lz} remap")
+lmp.command(f"run {500000}")  # Run in chunks of 500000 steps
+lmp.command(f"write_restart   restart.NVT")
