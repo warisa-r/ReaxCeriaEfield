fcc0da6bf5003f797c324f86b367711167ddf1a1
YiyangZhuu <yiyang.zhu@rwth-aachen.de>
2024-09-15 15:18:36 +0200

    fix_s_dump added

--- geometry_utils/__init__.py
+++ geometry_utils/__init__.py
@@ -3,3 +3,4 @@ from .generate_bulk import create_fluorite, create_rutile
 from .scale_geometry import scale_lammps_geometry
 from .visualize_geometry import convert_lammps_to_cif
 from .add_masses import add_masses_to_lmp
+from .fix_s_dump import fix_s_lammpstrj
