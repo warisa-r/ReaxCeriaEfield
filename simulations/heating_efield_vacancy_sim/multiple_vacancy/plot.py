345d4c9ebc7c75d8175b2f6f9efacb15bc549d3a
Andres Ramos <andres.ramos@graniewicz.com>
2024-09-21 17:49:54 +0200

    Added Plot

--- simulations/heating_efield_vacancy_sim/multiple_vacancy/plot.py
+++ simulations/heating_efield_vacancy_sim/multiple_vacancy/plot.py
@@ -6,11 +6,11 @@ from data_utils.extract_temperature import extract_temperature
 from data_utils.plotting import single_plot, double_plot
 
 x, y = extract_temperature("log.lammps")
-#Reduce for better visualization
-#x = x[:6000]
-#y = y[:6000]
+#Reduce to avoid error due to inclomplete log file, reduce for better visulization
+x = x[:15865]
+y = y[:15865]
 x_adjusted = x-x[0]
 
 np.savez('multiple_vacancy.npz', x=x_adjusted, y=y)
 
-single_plot(x_adjusted, y, 'Steps', 'Temperature', 'Multiple Vacancy', 'Multiple Vacancy.png')
+single_plot(x_adjusted, y, 'Steps', 'Temperature', 'Multiple Vacancy', 'Multiple_Vacancy.png')
